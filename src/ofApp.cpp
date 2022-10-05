aa90a4a35db05e22b6ad25ed51e6bb09f4202436
fabiolarobles1 <fabiola.robles1@upr.edu>
2022-10-05 18:42:00 -0400

    Car class completed

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,99 +1,83 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-
-    this->carXPos = 0;
-    this->carYPos = 0;
-    this->carSpeed = 5;
-    this->carDirection = 1;
-
+void ofApp::setup()
+{
+    firstCar = Car(0, 0, 5, 1);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update()
+{
 
-    if (((this->carDirection == 1) && (this->carXPos + 60 >= ofGetWidth())) ||
-        ((this->carDirection == -1) && (this->carXPos <= 0))) {
-
-        this->carDirection *= -1;
-    } else {
-        this->carXPos += this->carDirection * this->carSpeed;
+    if (((firstCar.getDirection() == 1) && (firstCar.getXpos() + firstCar.getWidth() >= ofGetWidth())) ||
+        ((firstCar.getDirection() == -1) && (firstCar.getXpos() <= 0)))
+    {
+        firstCar.setDirection(firstCar.getDirection() * -1);
+    }
+    else
+    {
+        firstCar.setXpos(firstCar.getXpos() + firstCar.getSpeed() * firstCar.getDirection());
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-
-    double x = this->carXPos;
-    double y = this->carYPos;
-    // Draw body
-    ofSetBackgroundColor(ofColor::white);
-    ofSetColor(ofColor::red);
-    ofDrawRectangle(x + 0, y + 10, 60, 10);
-    // Draw roof and windshields
-    ofDrawLine(x + 10, y + 10, x + 20, y + 0);
-    ofDrawLine(x + 20, y + 0, x + 40, y + 0);
-    ofDrawLine(x + 40, y + 0, x + 50, y + 10);
-
-    ofSetColor(ofColor(ofColor::black));
-    // Draw tires
-    ofDrawCircle(x + 15, y + 25, 5);
-    ofDrawCircle(x + 45, y + 25, 5);
-
+void ofApp::draw()
+{
+    firstCar.draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mouseDragged(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::mouseEntered(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+void ofApp::mouseExited(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 }
